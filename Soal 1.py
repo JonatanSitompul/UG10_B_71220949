037e551968f4318efdd766e4f29c1c8fa356b180
JonatanSitompul <117884416+JonatanSitompul@users.noreply.github.com>
2022-11-24 13:05:12 +0700

    Add files via upload

---
+++ Soal 1.py
@@ -0,0 +1,20 @@
+print ("Menghitung volume limas")
+pa = int(input("MAsukkan panjang alas: "))
+ta = int(input("Masukkan tinggi limas: "))
+
+Volume=1/3*(pa*ta)
+print ("Volume limas adalah =", Volume)
+
+print ("Menghitung Volume Bola")
+jari=int(input("Masukkan Jari-jari Bola: "))
+phi=(22/7)
+
+Volume=int((4/3)*phi*(jari*jari*jari))
+print ("Volume Bola =", Volume)
+
+print ("Menghitung Volume Prisma")
+pa = int(input("Masukkan Panjang Alas: "))
+ta = int(input("Masukkan Tinggi Alas: "))
+ts = int(input("Masukkan Tinggi Prisma"))
+
+Volume=int((Alas*Tinggi)/=2)*(Tinggi Prisma)
